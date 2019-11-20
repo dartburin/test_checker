31499bf6925f16a36d8141c68adddf0d4c982596
Dmitry Burin <dmitry_burin@epam.com>
2019-11-20 14:57:07 +0300

    correct errors and comment style

--- checker.go
+++ checker.go
@@ -3,22 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
 	"time"
 )
 
-/**
-@struct RetData Contained result of request
-*/
-type RetData struct {
-	RetCode int
+type RetData struct { // Result of request
+	bad     bool
+	timeOut bool
 	Time    time.Duration
 }
 
-/**
-@struct Statistics Contained result of test
-*/
-type Statistics struct {
+type Statistics struct { // Result of test
 	CntBad     int
 	CntTimeout int
 	MinTime    time.Duration
@@ -27,64 +24,56 @@ type Statistics struct {
 	FullTime   time.Duration
 }
 
-/**
-@brief Main function of checker
-@detailed This function parsed initial parameters, created requests, check result and
-calculated statistics
-*/
+// String is the Stringer interface of Statistics.
+func (s Statistics) String() string {
+	return fmt.Sprintf("\nResult statistic:\nCnt: bad=%v, timeout=%v\nTime: min=%v, middle=%v, max=%v, full=%v",
+		s.CntBad, s.CntTimeout, s.MinTime, s.MiddleTime, s.MaxTime, s.FullTime)
+}
+
+// Main function parsed initial parameters, created requests, check result and
+// calculated statistics
 func main() {
 	stat := Statistics{}
 	startTime := time.Now()
 
-	var domain string
-	flag.StringVar(&domain, "domain", "http://", "a domain name")
+	domain := flag.String("domain", "", "a domain name")
 
-	var cntRepeat int
-	flag.IntVar(&cntRepeat, "cnt", 4, "repeat requests count")
+	var cntRepeat uint
+	flag.UintVar(&cntRepeat, "cnt", 0, "repeat requests count - positive")
 
-	var timeout int
-	flag.IntVar(&timeout, "timeout", 1000000, "[timeout (ms)]")
+	var timeout uint
+	flag.UintVar(&timeout, "timeout", 0, "[timeout (ms) - optional, positive]")
 	flag.Parse()
 
-	/**
-	  Print arguments
-	*/
-	fmt.Println("")
-	fmt.Println("Check parameters:")
-	fmt.Printf("Domain: %v\n", domain)
-	fmt.Printf("Repeat count: %v\n", cntRepeat)
-
-	if timeout < 1000000 {
-		fmt.Printf("Timeout: %v\n", timeout)
+	// Check existing obligatory params
+	if *domain == "" || cntRepeat == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	fmt.Println("")
 
-	/**
-	  Start of test
-	*/
+	//  Start gorutines with http requests
 	c := make(chan RetData)
-	client := http.Client{}
+	client := http.Client{
+		Timeout: time.Second * time.Duration(timeout) / time.Duration(1000),
+	}
 
-	for i := 0; i < cntRepeat; i++ {
+	var i uint
+	for i = 0; i < cntRepeat; i++ {
 		go checkResource(domain, c, &client)
 	}
 
-	/**
-	  Result preparing
-	*/
-	checkTime := time.Second * time.Duration(timeout) / time.Duration(1000)
+	//  Statistic counting
 	len := time.Duration(0)
 	l := RetData{}
 
-	for i := 0; i < cntRepeat; i++ {
+	for i = 0; i < cntRepeat; i++ {
 		l = <-c
 
-		if l.RetCode < 200 || l.RetCode > 299 {
+		if l.bad {
 			stat.CntBad++
-			continue
 		}
 
-		if timeout > 0 && l.Time > checkTime {
+		if l.timeOut {
 			stat.CntTimeout++
 		}
 
@@ -100,46 +89,29 @@ func main() {
 	}
 
 	stat.FullTime = time.Since(startTime)
-	if cntRepeat-stat.CntBad != 0 {
-		stat.MiddleTime = len / time.Duration(cntRepeat-stat.CntBad)
-	} else {
-		stat.MiddleTime = 0
-	}
+	stat.MiddleTime = len / time.Duration(cntRepeat)
 
-	/**
-	  Print result
-	*/
-	fmt.Println("")
-	fmt.Println("Result statistic:")
-	fmt.Printf("CntBad: %v\n", stat.CntBad)
-	fmt.Printf("CntTimeout: %v\n", stat.CntTimeout)
-	fmt.Printf("MinTime: %v\n", stat.MinTime)
-	fmt.Printf("MaxTime: %v\n", stat.MaxTime)
-	fmt.Printf("MiddleTime: %v\n", stat.MiddleTime)
-	fmt.Printf("FullTime: %v\n", stat.FullTime)
-
-	fmt.Println("")
+	fmt.Println(stat) // Print result of request test
 }
 
-/**
-@brief Check availability of resource
-@param url Domain name
-@param channel Channel for exchange with main
-@param cl HTTP client
-*/
-func checkResource(url string, channel chan RetData, cl *http.Client) {
+// Check availability of resource
+func checkResource(url *string, channel chan RetData, cl *http.Client) {
 	ret := RetData{}
 	start := time.Now()
 
-	resp, err := cl.Get(url)
+	resp, err := cl.Get(*url)
 	if err != nil {
-		ret.RetCode = 500
+		ret.bad = true
+		if terr, ok := err.(net.Error); ok && terr.Timeout() {
+			ret.timeOut = true
+		}
 	} else {
-		ret.RetCode = resp.StatusCode
+		if resp.StatusCode < 200 || resp.StatusCode > 299 {
+			ret.bad = true
+		}
 		defer resp.Body.Close()
 	}
 
 	ret.Time = time.Since(start)
-	//fmt.Printf(" -- retCode %v\n", ret.RetCode)
 	channel <- ret
 }
